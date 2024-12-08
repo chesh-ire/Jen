363aacf56f286e18efc17c52b7d93f558f6ef4bc
Cheshire <126002367+SSHAARVARI@users.noreply.github.com>
2024-12-08 20:47:14 +0530

    fisrt commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// Example Node.js app (app.js)
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Dockerized App!');
+});
+
+app.listen(3000, () => {
+  console.log('App running on port 3000');
+});
